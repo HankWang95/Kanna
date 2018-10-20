3bab48a5a6b169d8efcab75cf48a2ff8feba8faf
HankWang95 <hankwang.whc@gmail.com>
2018-10-20 13:06:09 +0800

    u

--- server/mysql.go
+++ server/mysql.go
@@ -9,6 +9,7 @@ var db dbs.DB
 
 func InitMySQL() {
 	var err error
+	// todo 配置文件
 	db, err = dbs.NewSQL("mysql", "root:hankwang@tcp(127.0.0.1)/kanna?parseTime=true", 30, 5)
 	if err != nil{
 		log.Fatal(err)
--- services/notebook/word.go
+++ services/notebook/word.go
@@ -1,29 +1,31 @@
 package notebook
 
 import (
-	"time"
-	"github.com/smartwalle/dbs"
-	"github.com/HankWang95/Kanna/server"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net"
-	"log"
+	"github.com/HankWang95/Kanna/server"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/smartwalle/dbs"
+	"log"
+	"net"
+	"net/http"
 	"os"
+	"time"
 )
 
-
-
 type word struct {
-	Id           int64      `json:"id"                sql:"id"`
-	Word         string     `json:"word"              sql:"word"`
-	Translations string     `json:"translations"      sql:"translations"`
-	CreatedOn    *time.Time `json:"created_on"        sql:"created_on"`
-	AppearTime   int        `json:"appear_time"       sql:"appear_time"`
-	LastAppear   *time.Time `json:"last_appear"       sql:"last_appear"`
+	Id           int64                  `json:"id"                sql:"id"`
+	Word         string                 `json:"word"              sql:"word"`
+	Translations map[string]interface{} `json:"translations"      sql:"translations"`
+	CreatedOn    *time.Time             `json:"created_on"        sql:"created_on"`
+	AppearTime   int                    `json:"appear_time"       sql:"appear_time"`
+	LastAppear   *time.Time             `json:"last_appear"       sql:"last_appear"`
 }
-
+//
+//func (w *word) FormatTranslations() string {
+//	t := w.Translations
+//
+//}
 
 // ———————————————————————————————————————————— Loader -----------------------------------------------------
 
@@ -31,34 +33,35 @@ var queryWordChan = make(chan string, 10)
 
 // todo 统一装载 接口
 func queryWordEnter(word string) {
-	w, err := queryWord(word)
+	_, err := queryWord(word)
 	if err != nil {
-		fmt.Println(err)
+		logger.Println(err)
 	}
-	fmt.Println(w.Translations)
+	// todo 格式化输出
+	//os.Stdout.Write()
 }
-type wordLoader struct {}
+
+type wordLoader struct{}
 
 func NewWordLoader() *wordLoader {
 	return new(wordLoader)
 }
 
-func(this *wordLoader) LoadingFlag() (flagDict map[string]*chan string) {
+func (this *wordLoader) LoadingFlag() (flagDict map[string]*chan string) {
 	flagDict = make(map[string]*chan string)
 	flagDict["word"] = &queryWordChan
 	go flagHandler()
 	return
 }
 
-func flagHandler()  {
+func flagHandler() {
 	for {
 		select {
-		case word :=<- queryWordChan:
+		case word := <-queryWordChan:
 			queryWordEnter(word)
 		}
 	}
 }
-
 
 // ———————————————————————————————————————————— Service -----------------------------------------------------
 
@@ -66,24 +69,23 @@ var httpClient *http.Client
 var logger *log.Logger
 
 var (
-	MaxIdleConns = 100
+	MaxIdleConns        = 100
 	MaxIdleConnsPerHost = 100
 )
 
 func init() {
 	httpClient = createHTTPClient()
-	logFile,err := os.OpenFile("./kanna.log", os.O_WRONLY, os.ModeType)
+	logFile, err := os.OpenFile("./kanna.log", os.O_WRONLY, os.ModeType)
 	if err != nil {
-		fmt.Println(err)
+		logger.Fatal(err)
 	}
-	fmt.Printf(logFile.Name())
 	logger = log.New(logFile, "[kanna-notebook] ", log.Ltime|log.Ldate|log.Lshortfile)
 }
 
 func createHTTPClient() *http.Client {
 	client := &http.Client{
-		Timeout:time.Second*3,
-		Transport:&http.Transport{
+		Timeout: time.Second * 3,
+		Transport: &http.Transport{
 			Proxy: http.ProxyFromEnvironment,
 			DialContext: (&net.Dialer{
 				Timeout:   30 * time.Second,
@@ -91,12 +93,11 @@ func createHTTPClient() *http.Client {
 			}).DialContext,
 			MaxIdleConns:        MaxIdleConns,
 			MaxIdleConnsPerHost: MaxIdleConnsPerHost,
-			IdleConnTimeout:	 time.Second * 90,
+			IdleConnTimeout:     time.Second * 90,
 		},
 	}
 	return client
 }
-
 
 func youDaoTranslate(searchWord string) (result *word, err error) {
 	//requestUrl := fmt.Sprintf("http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=json&version=1.2&q=%s", KeyFrom, Key, searchWord)
@@ -128,30 +129,26 @@ func youDaoTranslate(searchWord string) (result *word, err error) {
 	resp, err := httpClient.Do(req)
 	defer resp.Body.Close()
 	if err != nil {
-		logger.Fatal("获取api出错" ,err)
+		logger.Fatal("获取api出错", err)
 	}
-	if err != nil {
-		return nil, err
-	}
+
 	if resp.Status != "200 OK" {
 		logger.Print("调用翻译api发生错误")
 		return nil, err
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	logger.Printf("search word : %s, YouDao API resp : %s", searchWord, "sss")
-
+	//body, err := ioutil.ReadAll(resp.Body)
 	var wordStruct = new(word)
 	wordStruct.Word = searchWord
-	wordStruct.Translations = string(body)
-	// todo 标准化word输出
+	var f interface{}
+	err = json.NewDecoder(resp.Body).Decode(&f)
+	if err != nil {
+		logger.Println(" decode err: ", err)
+		return
+	}
+	wordStruct.Translations = f.(map[string]interface{})
+
 	err = sqlAddWord(wordStruct)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return wordStruct, nil
@@ -175,11 +172,9 @@ func queryWord(searchWord string) (result *word, err error) {
 	return result, nil
 }
 
-
 // ———————————————————————————————————————————— MySQL -----------------------------------------------------
 
-
-const(
+const (
 	K_DB_NOTEBOOK_WORD = "notebook_word"
 )
 
@@ -214,7 +209,6 @@ func sqlUpdateWord(id int64) (err error) {
 	ub.SET("last_appear", time.Now())
 	ub.Table(K_DB_NOTEBOOK_WORD)
 	ub.Where("id = ?", id)
-	fmt.Println(ub.ToSQL())
 	_, err = ub.Exec(server.GetMySQLSession())
 	return
 }
